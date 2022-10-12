1837240ee31594109b0bc3f460e1aa549bf784c4
raihananhar <203040113@mail.unpas.ac.id>
2022-10-12 17:07:48 +0700

    Penambahan file

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -1,8 +1,22 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
     public function __construct()
     {
-        echo 'HAI!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if( isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
