2572cbd6a192dcb98ca0bdcbba2940872042fbd8
raihananhar <203040113@mail.unpas.ac.id>
2022-10-12 20:22:32 +0700

    Penambahan file js

--- mvc/public/js/script.js
+++ mvc/public/js/script.js
@@ -9,13 +9,13 @@ $(function() {
 
         $('#forModalLabel').html('Ubah Data Mahasiswa');
         $('.modal-footer button[type=submit]').html('Ubah Data');
-        $('.modal-body form').attr('action', 'http://localhost/prakweb_b_203040102/mvc/public/mahasiswa/ubah');
+        $('.modal-body form').attr('action', 'http://localhost/prakweb_b_203040113/mvc/public/mahasiswa/ubah');
 
         const id = $(this).data('id');
         
 
         $.ajax({
-            url: 'http://localhost/prakweb_b_203040102/mvc/public/mahasiswa/getubah',
+            url: 'http://localhost/prakweb_b_203040113/mvc/public/mahasiswa/getubah',
             data: {id : id},
             method : 'post',
             dataType : 'json',
