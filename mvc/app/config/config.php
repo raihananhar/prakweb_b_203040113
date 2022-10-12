17ab3f92dfeb11b868924c4dfad806a051c3e2fc
raihananhar <203040113@mail.unpas.ac.id>
2022-10-12 20:06:16 +0700

    Penambahan file config

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb_b_203040113/mvc/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb_b_203040113');
