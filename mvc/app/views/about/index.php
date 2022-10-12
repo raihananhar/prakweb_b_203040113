f9b30faa990a6e265867f0a03b59ad5b5ccb2fe7
raihananhar <203040113@mail.unpas.ac.id>
2022-10-12 18:26:13 +0700

    Penambahan file view

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
     <h1 mt-4 class="text-light">About me</h1>
-    <img src="<?= BASEURL; ?>/img/agung1.jpg" alt="Raihan Anhar" width="200" class="rounded-circle shadow"> 
+    <img src="<?= BASEURL; ?>/img/RaihanAnhar.jpg" alt="Raihan Anhar" width="200" class="rounded-circle shadow"> 
     <p class="text-light">Halo, nama saya <?=$data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
 </div>
