e6048a150d30eea2966a57bdf62f8d0dd38bec57
raihananhar <203040113@mail.unpas.ac.id>
2022-10-14 13:16:17 +0700

    Pengeditan file header

--- mvc/app/views/templates/header.php
+++ mvc/app/views/templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
     <link href="<?= BASEURL; ?>/css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/myCSS/header2.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/myCSS/header.css">
 </head>
 <body>
 
